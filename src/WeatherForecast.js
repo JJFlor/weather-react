07a5030bd02aab14b2b7634eba93dd2110c49024
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2021-04-30 16:03:09 +0200

    Changed Map functionality to forEach in the loop

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,7 +16,7 @@ export default function WeatherForecast(props){
         return(
             <div className="WeatherForecast">
             <div className="row">
-                {forecast.map(function(dailyForecast, index){
+                {forecast.forEach(function(dailyForecast, index){
                     if(index < 5){
                         return(
                         <div className="col" key={index}>
