da43e5c67353bf36c09e901a1c9b3c1f38e78e1a
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2021-04-30 15:58:58 +0200

    Added a loop to display 5 days forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -13,13 +13,18 @@ export default function WeatherForecast(props){
    }
   
     if(loaded) {
-        console.log(forecast);
         return(
             <div className="WeatherForecast">
             <div className="row">
-                <div className="col">
-                    <WeatherForecastDay data={forecast[0]}/>
-                </div>
+                {forecast.map(function(dailyForecast, index){
+                    if(index < 5){
+                        return(
+                        <div className="col" key={index}>
+                          <WeatherForecastDay data={dailyForecast}/>
+                        </div>
+                    );
+                    }  
+                })}
             </div>
         </div>
         );
